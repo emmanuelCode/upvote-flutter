9fa2f356b18f815b47b3321d55c14dc588f7d375
emmanuelCode <emmanuelcode@outlook.com>
2023-03-17 13:21:56 -0400

    remove unnecessary file

--- lib/View/upvote_provider.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:riverpod_annotation/riverpod_annotation.dart';
-// import 'package:simplemviprovider/Intent/upvote_intent.dart';
-
-// part 'upvote_provider.g.dart';
-
-// @riverpod
-// MainViewIntentFactory intentFactory(IntentFactoryRef ref) {
-//   return MainViewIntentFactory();
-// }
