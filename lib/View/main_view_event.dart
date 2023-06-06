c9d550662281da744e8f8715104025b5e93991b9
emmanuelCode <emmanuelcode@outlook.com>
2023-06-05 20:55:54 -0400

    chore: update to sealed class using dart 3

--- lib/View/main_view_event.dart
+++ lib/View/main_view_event.dart
@@ -1,16 +1,8 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'main_view_event.freezed.dart';
-
 // this is the same as a sealed class in kotlin
 // it similar to enums but have more functionnality
 // some example: https://www.baeldung.com/kotlin/sealed-class-vs-enum
-// we used the freezed pakage to generate it for a dart version
 
-@freezed
-class MainViewEvent with _$MainViewEvent {
-  
-  const factory MainViewEvent.thumbsUpClick() = ThumbsUpClick;
- 
-  const factory MainViewEvent.loveItClick() = LoveItClick;
-}
+sealed class MainViewEvent {}
+
+class ThumbsUpClick extends MainViewEvent {}
+class LoveItClick extends MainViewEvent {}
